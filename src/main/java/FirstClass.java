94d7d481e2c769e14e0724809a64b3675da18c9c
Dereck-Baksa <dereck.baksa@sptech.school>
2025-06-12 14:49:18 -0300

    HELLO JAVA

---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,16 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello java world");
+
+// tipos que mais
+        String nome = "Dereck";
+        Integer idade = 19;
+        Double altura = 1.84;
+        Boolean bloqueado = false;
+
+
+
+        System.out.println("Olá meu nome é " + nome + " e tenho " + idade + " anos de idade, minha altura e " + altura + " estou feliz por estar aqui." );
+
+    }
+}
