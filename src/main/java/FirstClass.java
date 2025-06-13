7ca136164f2446675cb70a892d59fc7421bb23ce
Dereck-Baksa <dereck.baksa@sptech.school>
2025-06-13 11:19:51 -0300

    HELLO JAVA mais funções matemáticas

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -12,5 +12,49 @@ public class FirstClass {
 
         System.out.println("Olá meu nome é " + nome + " e tenho " + idade + " anos de idade, minha altura e " + altura + " estou feliz por estar aqui." );
 
+        exibirSoma(10.0,6.0);
+        exibirSub(2.0,1.0);
+        exibirDivisao(10.0,5.0);
+        exibirMultiplicacao(10.0,5.0);
+        exibirPotencia(2.0,3.0);
+        // exibirSomaComNome(10.0,9.0,nome);
     }
+
+static void exibirSoma(Double a, Double b){
+        Double soma = a + b ;
+    System.out.println(" A soma de " + a + " + " +  b  + " é igual a " + soma);
+
+}
+
+static void exibirSub(Double a, Double b){
+        Double sub = a - b ;
+System.out.println(" A sub de " + a + " - " +  b  + " é igual a " + sub);
+
+}
+static void exibirDivisao(Double a,Double b){
+        Double divisao = a / b;
+    System.out.println(" A divisão de " + a + " e " + b + " e igual a " + divisao);
+}
+
+static void exibirMultiplicacao(Double a,Double b){
+        Double multiplicacao = (a * b);
+    System.out.println(" A multiplicação de " + a + " e " + b + " é igual a " + multiplicacao) ;
+
+}
+static void exibirPotencia(Double a,Double b){
+        Double potencia = Math.pow(a, b);
+    System.out.println(" O número " + a + " potenciado a " + b + " é igual a " + potencia );
+}
+
+//    static void exibirSomaComNome(Double a, Double b, String nome){
+//        Double soma = a + b ;
+//        System.out.println("A soma é " + soma + " e quem solicitou foi o " + nome);
+//
+//    }
+
+
+
+
+
+
 }
